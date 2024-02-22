c753933fb68476c95456229e3c419b4d3d30ad7a
brianmujjuni <brianmujjuni@engineer.com>
2024-02-22 12:41:17 +0300

    add toggle function to change state of isOpen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const faqs = [
   {
     title: "Where are these chairs assembled?",
@@ -32,12 +34,16 @@ return(
 }
 
 function AccordionItem({num,title,text}){
+  const [isOpen, setIsOpen] = useState(false)
+  function handleToggle(){
+    setIsOpen((isOpen)=>!isOpen)
+  }
 return(
-  <div className="item">
+  <div className="item" onClick={handleToggle}>
     <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
     <p className="text">{title}</p>
-    <p className="icon">-</p>
-    <div className="content-box">{text}</div>
+    <p className="icon">{isOpen ? '-' : '+'}</p>
+    {isOpen && <div className="content-box">{text}</div>}
   </div>
 )
 }
